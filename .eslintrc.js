4a6696a030cf71c65b9cece1dd644f44858a0ec0
Ясен Янев <124697579+YasenYanev@users.noreply.github.com>
2024-09-12 16:58:05 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["airbnb-base", "prettier"],
+  rules: {
+    "import/no-extraneous-dependencies": "off",
+    "import/order": "off",
+    "prefer-destructuring": "off",
+    "import/extensions": "off",
+    "no-unused-vars": "off",
+    "no-param-reassign": "off",
+    "no-else-return": "off",
+    "no-lonely-if": "off",
+    "max-classes-per-file": "off",
+  },
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# Clone the repository
+
+git clone git@github.com:YasenYanev/template-repo.git
+
+# Navigate to the project directory
+
+cd yourproject
+
+# Install dependencies
+
+npm install
