eac9ef044d7428623bca410805d2c707c027cfbd
Yasen Yanev <124697579+YasenYanev@users.noreply.github.com>
2024-09-16 16:46:59 +0300

    Form validation done Complete remake of javascript code

--- src/index.js
+++ src/index.js
@@ -1,75 +1,71 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
 import "./styles/main.css";
 
 const inputs = document.querySelectorAll("input");
+const passwordInputs = document.querySelectorAll("input[type='password']");
 const errMessage = document.querySelector(".formatErrorMsg");
 const notEqualErrorMsg = document.querySelector(".notEqualErrorMsg");
-const regExpresions = {
-  text: /^[^A-Z]*[A-Z][^A-Z]*$/,
-  email: /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,6}$/,
-  password:
-    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-  tel: /^\d{3}-\d{3}-\d{4}$/,
-};
 
-function validateInputs(e) {
-  const inputEL = e.target;
-  if (regExpresions[inputEL.type].test(inputEL.value)) {
-    inputEL.classList.remove("invalid-input");
-    inputEL.classList.add("valid-input");
-    return true;
-  } else {
-    inputEL.classList.remove("valid-input");
-    inputEL.classList.add("invalid-input");
-    return false;
+class StateController {
+  constructor() {
+    this.regExpresions = {
+      text: /^[^A-Z]*[A-Z][^A-Z]*$/,
+      email: /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,6}$/,
+      password:
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      tel: /^\d{3}-\d{3}-\d{4}$/,
+    };
+
+    this.states = {
+      firstName: true,
+      lastName: true,
+      email: true,
+      password: true,
+      confirmPassword: true,
+      phoneNum: true,
+      samePassword: true,
+    };
+  }
+
+  validateInput(input) {
+    this.states[input.name] = this.regExpresions[input.type].test(input.value);
+
+    if (
+      passwordInputs[0].value !== passwordInputs[1].value &&
+      passwordInputs[1].value.length > 0
+    ) {
+      this.states.samePassword = false;
+    } else {
+      this.states.samePassword = true;
+    }
+  }
+
+  renderState(e) {
+    const input = e.target;
+    this.validateInput(input);
+
+    errMessage.classList.remove("active");
+    notEqualErrorMsg.classList.remove("active");
+    input.classList.remove("invalid-input", "valid-input");
+
+    if (this.states[input.name]) {
+      input.classList.add("valid-input");
+    } else {
+      if (input.name === "password" && e.type !== "blur") {
+        errMessage.classList.add("active");
+      }
+      input.classList.add("invalid-input");
+    }
+
+    if (!this.states.samePassword) {
+      notEqualErrorMsg.classList.add("active");
+    }
   }
 }
 
-function validatePasswords(e) {
-  const passwordInputs = [...inputs].filter(
-    (input) => input.type === "password",
-  );
-
-  errMessage.classList.remove("active");
-  notEqualErrorMsg.classList.remove("active");
-
-  //   if (
-  //     passwordInputs[0].value !== passwordInputs[1].value &&
-  //     passwordInputs[1].value.length > 0
-  //   ) {
-  //     passwordInputs[0].classList.remove("valid-input");
-  //     passwordInputs[0].classList.add("invalid-input");
-  //     passwordInputs[1].classList.remove("valid-input");
-  //     passwordInputs[1].classList.add("invalid-input");
-  //   } else if (!validateInputs(e)) {
-  //     errMessage.classList.add("active");
-  //   }
-
-  if (!validateInputs(e)) {
-    errMessage.classList.add("active");
-    console.log(true);
-  } else if (
-    passwordInputs[0].value !== passwordInputs[1].value &&
-    passwordInputs[1].value.length > 0
-  ) {
-    passwordInputs[0].classList.remove("valid-input");
-    passwordInputs[0].classList.add("invalid-input");
-    passwordInputs[1].classList.remove("valid-input");
-    passwordInputs[1].classList.add("invalid-input");
-
-    notEqualErrorMsg.classList.add("active");
-  }
-}
+const controller = new StateController();
 
 inputs.forEach((input) => {
-  if (input.type === "password") {
-    input.addEventListener("input", validatePasswords);
-    input.addEventListener("focus", validatePasswords);
-    input.addEventListener("focusout", validatePasswords);
-    return;
-  }
-
-  input.addEventListener("input", validateInputs);
-  input.addEventListener("focus", validateInputs);
+  ["input", "focus", "blur"].forEach((event) =>
+    input.addEventListener(event, (e) => controller.renderState(e)),
+  );
 });
